27da9aec6b4871613e22176bcbe8d868e6c01ae5
vishnu <vishnu452@gmail.com>
2026-09-14 15:24:44 +0530

    add vitest runner instead of storybook runner

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import react from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vitest/config';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -33,6 +35,30 @@ export default defineConfig({
             'stories/**/*.stories.{js,jsx}',
           ],
           globals: true,
+        },
+      },
+      {
+        extends: true,
+        plugins: [
+          storybookTest({
+            configDir: path.resolve(dirname, '.storybook'),
+          }),
+        ],
+        resolve: {
+          alias: {
+            '@src': path.resolve(dirname, 'src'),
+            '@stories': path.resolve(dirname, 'stories'),
+          },
+        },
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            headless: true,
+            instances: [{ browser: 'chromium' }],
+          },
+          setupFiles: ['./.storybook/vitest.setup.ts'],
         },
       },
     ],
